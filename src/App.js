7085ec35499befe25c86954dd386808f6009f73c
rcshpark <106796843+rcshpark@users.noreply.github.com>
2022-07-08 18:12:47 +0900

    refactor : 불필요 파일 소스코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation></Navigation>
       프로젝트 시작
     </div>
   );
