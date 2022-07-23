690db2f42c3edf3566c7d8d3a300932e4b9f4d08
rcshpark <106796843+rcshpark@users.noreply.github.com>
2022-07-24 01:38:57 +0900

    fix: mock 데이터를 홈화면에 표시

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled  from 'styled-components';
 
-const ThemeButton = ({themeName}) => {
-    return <ThemeButtonStyled>{themeName}</ThemeButtonStyled>;
+const ThemeButton = ({ themeName, onClick }) => {
+    return <ThemeButtonStyled onClick={onClick}>{themeName}</ThemeButtonStyled>;
 };
 
 const ThemeButtonStyled = styled.div`
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Navigation from '../components/Navigation';
 import ThemeButton from '../components/ThemeButton';
 import ProductCard from '../components/ProductCard';
 import styled from 'styled-components';
+import { mockTheme1Produdcts,mockTheme2Produdcts } from '../data/mockData';
 
 const Home = () => {
     return ( 
@@ -11,33 +12,37 @@ const Home = () => {
         <Navigation/>
         <ThemeLine></ThemeLine>
         <ThemeSection>
-        <ThemeButton themeName = {"#겨울 방한템"}/>
-        <ThemeButton themeName = {"#따순 머그컵"}/>
+        <ThemeButton
+             themeName={"#따순머그컵"}
+             onClick={() => onClickThemeButton(1)}
+           />
+           <ThemeButton
+             themeName={"#여름엔바로"}
+             onClick={() => onClickThemeButton(2)}
+           />
         </ThemeSection>
         <GrayLine/>
       </div>
-     <div>       
-        <ProductSection>
-        <ProductCard 
-        name='비숑 블랙 머그잔' 
-        description={'쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은\n 블랙 & 화이트 비숑 머그잔입니다.'} 
-        thumbnail='https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg'/>
-
-        <ProductCard 
-        name='가열 보온 티코스터 온열 원터치 컵 받침대' 
-        description='언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.' 
-        thumbnail='https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg'/>
-
-        <ProductCard 
-        name='벨루즈까사 솜사탕 파스텔 머그 4종 세트' 
-        description='솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그' 
-        thumbnail='https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg'/>
+      <ProductSection>
+        {/* return내부는 html문법이 들어가므로 기본적인 if문을 사용할 수 없음. */}
+        {products ? (
+           products.map((product) => (
+             <ProductCard
+               key={product.id}
+               name={product.name}
+               description={product.description}
+               thumbnail={product.thumbnail}
+             />
+           ))
+         ) : (
+           <div>테마를 선택해주세요</div>
+         )}
         </ProductSection>
      </div>
-    </div>
-
     );
-};
+         };
+
+
 
 const ThemeLine = styled.div`
     position: absolute;
