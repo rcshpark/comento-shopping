345dcfa2c3259a578350770e325265d8378e18b7
rcshpark <106796843+rcshpark@users.noreply.github.com>
2022-07-24 02:00:04 +0900

    Refactor: 불필요 코드 제거 design: 상품상세페이지 스타일 틀 구성

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,44 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navigation from '../components/Navigation';
-import DetailCard from '../components/DetailCard';
-import ProductButton from '../components/ProductButton';
-import ProductButton2 from '../components/ProductButton2';
+import { useParams } from "react-router-dom";
+import { getProductDetail } from "../data/mockData";
+import { useEffect, useState } from "react";
+
+
+
 
 const ProductDetail = () => {
+    let {productId} = useParams();
+    const [product, setProduct] = useState();
+
+   useEffect(() => {
+     const result = getProductDetail(productId);
+     setProduct(result);
+   }, []);
     return (
-    <div>
-    <div>
-        <Navigation/>
-    </div>
-   <div>
-        <ProductSection>
-        <DetailCard
-        name='비숑 블랙 머그잔'
-        price='21,800원'
-        thumbnail='https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg'/>
-        </ProductSection>
-   </div>
-   <div>
-        <ThemeSection>
-        <ProductButton themeName={"상품 설명"}/>
-        <ProductButton2 themeName={"상품 후기"}/>
-        </ThemeSection>
-   </div>
-   </div>
-    )
+        <ProductDetailStyled>
+          <Navigation />
+        </ProductDetailStyled>
+      );
 };
-const ProductSection = styled.div`
-margin-top: 60px;
-`;
 
-
-const ThemeSection = styled.div`
-    display:flex;
-    gap:0px;
-    padding: 24px 0px;
-
-
-`;
-
+const ProductDetailStyled = styled.div``;
 export default ProductDetail;
